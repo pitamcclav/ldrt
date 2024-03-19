718301d1afe6d94e8a94f7ad503d622c7b5b171d
Peter Claver <pitamcclav@gmail.com>
2024-03-20 01:43:17 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
-  /**
-   * Mobile nav toggle
-   */
-
-  // const mobileNavShow = document.querySelector('.mobile-nav-show');
-  // const mobileNavHide = document.querySelector('.mobile-nav-hide');
-
-  // document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
-  //   el.addEventListener('click', function(event) {
-  //     event.preventDefault();
-  //     mobileNavToogle();
-  //   })
-  // });
-
-  // function mobileNavToogle() {
-  //   document.querySelector('body').classList.toggle('mobile-nav-active');
-  //   mobileNavShow.classList.toggle('d-none');
-  //   mobileNavHide.classList.toggle('d-none');
-  // }
-
-  // /**
-  //  * Hide mobile nav on same-page/hash links
-  //  */
-  // document.querySelectorAll('#navbar a').forEach(navbarlink => {
-
-  //   if (!navbarlink.hash) return;
-
-  //   let section = document.querySelector(navbarlink.hash);
-  //   if (!section) return;
-
-  //   navbarlink.addEventListener('click', () => {
-  //     if (document.querySelector('.mobile-nav-active')) {
-  //       mobileNavToogle();
-  //     }
-  //   });
-
-  // });
 
   /**
    * Toggle mobile nav dropdowns
