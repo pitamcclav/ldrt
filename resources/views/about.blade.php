74d959492964a87545eb07ba9b887232b92f551d
Peter Claver <pitamcclav@gmail.com>
2024-03-19 12:50:11 +0300

    Revert "home page and about page changes"
    This reverts commit 7199b86d471256a609800f1d695973d836325aac.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,22 +22,17 @@
       <div class="col-lg-7 about-img" style="background-image: url(assets/img/about.jpg);"></div>
 
       <div class="col-lg-7">
-        <h2>Lugazi Rural Financial Development Trust </h2>
+        <h2>Consequatur eius et magnam</h2>
         <div class="our-story">
-          <h4>Est 2014</h4>
+          <h4>Est 1988</h4>
           <h3>Our Story</h3>
-          <p> Established in 2014, Lugazi Rural Development Trust (LRDT) is an organization found in Namataba
- town council of Mukono District.  The organization came into existence as a project implemented under the theme of Nutrition, Health
- and Economic status improvement at household level that comprised of commercial vegetable growing
- and value addition. Over the years, LRFDT has grown together with its beneficiaries and elevated from
- household level to group level</p>
+          <p>Inventore aliquam beatae at et id alias. Ipsa dolores amet consequuntur minima quia maxime autem. Quidem id sed ratione. Tenetur provident autem in reiciendis rerum at dolor. Aliquam consectetur laudantium temporibus dicta minus dolor.</p>
           <ul>
-          <h5>OUR ACHIEVEMENTS</h5>
-            <li><i class="bi bi-check-circle"></i> <span> <b>LRFDT Livelihoods Project:</b> A project aimed at improving livelihoods, directly benefiting the community.</span></li>
-            <li><i class="bi bi-check-circle"></i> <span> <b>GENINSA Projects: </b>Collaborative projects focusing on agriculture, entrepreneurship, hydroponic farming, and pharmacological characterization</span></li>
-            <li><i class="bi bi-check-circle"></i> <span> <b>MUWRP Health Promotion: </b>Sensitization and awareness initiatives on health-related issues.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commo</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Duis aute irure dolor in reprehenderit in</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
           </ul>
-          <p>Collaboration with various stakeholders and partners further amplified our impact. LRFDT is committed to creating lasting change and uplifting rural communities in Uganda.</p>
+          <p>Vitae autem velit excepturi fugit. Animi ad non. Eligendi et non nesciunt suscipit repellendus porro in quo eveniet. Molestias in maxime doloremque.</p>
 
           <div class="watch-video d-flex align-items-center position-relative">
             <i class="bi bi-play-circle"></i>
